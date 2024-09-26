da29541d5903e84fd6dfc0a96f2ed12f5770ffa0
Nikunj Patel <96856377+nrrpatel@users.noreply.github.com>
2024-09-26 03:18:25 -0400

    added OTP for admin access

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import PasskeyModal from "@/components/PasskeyModal";
 import { Button } from "@/components/ui/button";
 import PatientForm from "@/components/ui/forms/PatientForm";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home( {searchParams}: SearchParamProps) {
+  const isAdmin = searchParams.admin === 'true';
   return(
     <div className="flex h-screen max-h-screen">
-      {/* TOD0: OTP Verification */}
+      {isAdmin && <PasskeyModal/>}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image 
